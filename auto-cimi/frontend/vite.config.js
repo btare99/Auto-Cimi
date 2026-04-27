ff8a22b30854054dc6ff34897ac3a04f2a3a1037
Bjorni Tare <btare99@gmail.com>
2026-04-27 20:42:25 +0200

    commi

--- auto-cimi/frontend/vite.config.js
+++ auto-cimi/frontend/vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5005',
+        target: 'http://127.0.0.1:5005',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
